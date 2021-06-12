27e1b79267a2b254918f102706259703a0595fdd
Ben Crowe <benjamin.crowe@comcast.net>
2021-06-12 16:01:34 -0600

    updatedNextLastPrevious

--- controllers/w08.js
+++ controllers/w08.js
@@ -6,22 +6,29 @@ const ITEMS_PER_PAGE = 10 // Limit of 10 items per page.
 
 const renderIndex = (req, res, json) => {
     let searchedValue = req.body.searchValue || req.query.searchValue || '' // Handle for GET, POST or neither
-    let page = req.query.page || 1 // Grab our page number, 1 if undefined
+    let page = +req.query.page || 1 // Grab our page number, 1 if undefined
 
     const indexStart = (page - 1) * ITEMS_PER_PAGE // Item index to start on...
     const indexEnd = page * ITEMS_PER_PAGE
+    let totalItems; 
 
     const filteredData = global.jsonResponse.filter(x =>
         x.name.toLowerCase().includes(searchedValue.toLowerCase())
     )
-
+        totalItems = filteredData.length;
     let stuff = {
         data: filteredData.slice(indexStart, indexEnd), // For JSON/Array and not Mongoose, .slice() works best.
         path: 'proveAssignments/03',
         title: 'Lesson 3 Prove Assignment',
         searchedValue: searchedValue,
+        totalProducts: totalItems,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < filteredData,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
         page: page,
-        numPages: Math.ceil(filteredData.length / ITEMS_PER_PAGE)
+        lastPage: Math.ceil(filteredData.length / ITEMS_PER_PAGE)
     }
 
     res.render('pages/w08', stuff)
